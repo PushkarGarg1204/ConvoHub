3192dce65354f1cea5e11d43c66a15b6b2c59b2f
PushkarGarg1204 <gargpushkar1204@gmail.com>
2026-07-31 10:57:49 +0530

    Added other authentication api's

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import express from "express";
+import express from "express"; // method-2
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config({});
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+// routes
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
