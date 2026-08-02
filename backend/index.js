e6cae8990ee9067a7a7f175eaf33eb946ce83f1a
PushkarGarg1204 <gargpushkar1204@gmail.com>
2026-08-02 13:55:04 +0530

    Convert backend from submodule to regular directory

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express"; // method-2
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+import userRoute from "./routes/userRoute.js";
+import cookieParser from "cookie-parser";
+import messageRoute from "./routes/messageRoute.js";
+import cors from "cors";
+
+dotenv.config({});
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+// routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log(`Server listen at port ${PORT}`);
+});
